72ff21fb1049229395bad6135f5fdaa4d2854d53
gaoyike <gaoyike!@gmail.com>
2019-01-25 13:24:39 -0600

    educational round 1

---
+++ main/readman/TaskA.java
@@ -0,0 +1,11 @@
+package readman;
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+
+public class TaskA {
+    public void solve(int testNumber, Scanner in, PrintWriter out) {
+
+    }
+}
